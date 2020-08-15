031216c0872dac4338cca338766526e7b7513563
kai618 <vanhieu1601@gmail.com>
2020-08-15 10:51:47 +0700

    improved collider box and added event system into touch detector

--- Assets/Scripts/Controller/TouchDetector.cs
+++ Assets/Scripts/Controller/TouchDetector.cs
@@ -34,6 +34,13 @@ public class TouchDetector : MonoBehaviour
 
     private void CheckUserTouch()
     {
+        if (Input.touches.Length > 1)
+        {
+            foreach(Touch t in Input.touches)
+            {
+                Debug.Log(t + " || " + t.fingerId);
+            }
+        }
         if (Input.touchCount == 1)
         {
             Touch touch = Input.touches[0];
