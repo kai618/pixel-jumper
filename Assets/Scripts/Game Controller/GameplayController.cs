85ddfc993d59385fb832b31a5530f898f8090ffb
kai618 <vanhieu1601@gmail.com>
2020-08-17 19:23:22 +0700

    added 2 zoom in and zoom out buttons to pause panel in level scene

--- Assets/Scripts/Game Controller/GameplayController.cs
+++ Assets/Scripts/Game Controller/GameplayController.cs
@@ -8,14 +8,22 @@ public class GameplayController : MonoBehaviour
 {
 #pragma warning disable 0649
     [SerializeField] private GameObject pausePanel;
+    [SerializeField] private Button zoomInBtn;
+    [SerializeField] private Button zoomOutBtn;
 #pragma warning disable 0649
 
     private TouchDetector touchDetector;
+    private Camera levelCamera;
+    private readonly float minCameraSize = 5;
+    private readonly float maxCameraSize = 7;
     private GameObject pauseBtn;
+
 
     void Awake()
     {
         touchDetector = GameObject.Find("Touch Detector").GetComponent<TouchDetector>();
+        levelCamera = GameObject.Find("Level Camera").GetComponent<Camera>();
+
         pauseBtn = GameObject.Find("Pause Btn");
     }
 
@@ -41,4 +49,36 @@ public class GameplayController : MonoBehaviour
         SceneManager.LoadScene("MenuScene");
     }
 
+    public void ZoomIn()
+    {
+        float size = levelCamera.orthographicSize;
+        if (size > minCameraSize) size--;
+        levelCamera.orthographicSize = size;
+        ChangeZoomBtnState(size);
+    }
+
+    public void ZoomOut()
+    {
+        float size = levelCamera.orthographicSize;
+        if (size < maxCameraSize) size++;
+        levelCamera.orthographicSize = size;
+        ChangeZoomBtnState(size);
+    }
+
+    private void ChangeZoomBtnState(float cameraSize)
+    {
+        if (cameraSize == minCameraSize)
+        {
+            zoomInBtn.interactable = false;
+        }
+        else if (cameraSize == maxCameraSize)
+        {
+            zoomOutBtn.interactable = false;
+        }
+        else
+        {
+            zoomInBtn.interactable = true;
+            zoomOutBtn.interactable = true;
+        }
+    }
 }
